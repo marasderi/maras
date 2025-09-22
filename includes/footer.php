1d1163e29b31b790022f3f0f27f1fff6fcb249ea
Maraş Deri <marasderi@gmail.com>
2025-09-22 16:39:36 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,5 +4,7 @@
         <p>&copy; <?php echo date('Y'); ?> Deri Pazarı. Tüm hakları saklıdır.</p>
     </div>
 </footer>
+<script src="<?php echo SITE_URL; ?>/assets/js/main.js"></script>
+
 </body>
 </html>
