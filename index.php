2f89ed68cf315c40d9af850519b4d9e3dcc07956
Maraş Deri <marasderi@gmail.com>
2025-09-22 13:43:41 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php'; ?>
+
+<div style="text-align: center; padding: 40px 0;">
+    <h2>Sitemizin Temeli Hazır!</h2>
+    <p>
+        Veritabanı bağlantısı ve temel dosya yapısı tamamlandı.<br>
+        Artık üyelik sistemini ve diğer özellikleri eklemeye başlayabiliriz.
+    </p>
+</div>
+
+<?php include 'includes/footer.php'; ?>
